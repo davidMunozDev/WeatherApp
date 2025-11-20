465dc4908ef8ed62cd5179c207034d90c7fb42c2
davidMunozDev <davidmunozdev@gmail.com>
2025-11-20 22:02:26 +0100

    Update app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'detail', component: DetailComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '**', redirectTo: 'home' },
+];
---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detail.component.html',
+})
+export class DetailComponent {}
